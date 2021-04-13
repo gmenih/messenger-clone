6f5de8a176489a4f2d3eda4dd253a1e277c15745
Gregor Menih <gregor@menih.si>
2021-04-13 21:54:25 +0200

    destroy

--- src/app/messenger/page/chat-page.component.ts
+++ src/app/messenger/page/chat-page.component.ts
@@ -1,6 +1,6 @@
-import {Component, OnInit} from '@angular/core';
-import {Observable} from 'rxjs';
-import {take} from 'rxjs/operators';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Observable, ReplaySubject, Subject} from 'rxjs';
+import {take, takeUntil} from 'rxjs/operators';
 import {DisplayableMessage} from '../services/types/conversation.types';
 import {AuthFacade} from '../store/auth/auth.facade';
 import {ConversationFacade} from '../store/conversation/conversation.facade';
@@ -10,10 +10,12 @@ import {ConversationFacade} from '../store/conversation/conversation.facade';
     templateUrl: './chat-page.component.html',
     styleUrls: ['./chat-page.component.scss'],
 })
-export class ChatPageComponent implements OnInit {
+export class ChatPageComponent implements OnInit, OnDestroy {
     public receivedMessage: DisplayableMessage[] = [];
     public activeQuestionId?: string;
     public isLoading$: Observable<boolean>;
+
+    public destroyed$: ReplaySubject<void> = new ReplaySubject<void>(1);
 
     constructor (
         private readonly authFacade: AuthFacade,
@@ -24,20 +26,27 @@ export class ChatPageComponent implements OnInit {
 
     public ngOnInit (): void {
         this.authFacade.authTokens$
-            .pipe(take(1))
+            .pipe(take(1), takeUntil(this.destroyed$))
             .subscribe((authToken) => {
                 this.conversationFacade.startConversation(authToken);
             });
 
         this.conversationFacade.activeQuestionId$
+            .pipe(takeUntil(this.destroyed$))
             .subscribe(activeQuestionId => {
                 this.activeQuestionId = activeQuestionId ?? undefined;
             });
 
         this.conversationFacade.messages$
+            .pipe(takeUntil(this.destroyed$))
             .subscribe(messages => {
                 this.receivedMessage = messages;
             });
+    }
+
+    public ngOnDestroy (): void {
+        this.destroyed$.next();
+        this.destroyed$.complete();
     }
 
     public trackById (_: number, message: DisplayableMessage): string {
